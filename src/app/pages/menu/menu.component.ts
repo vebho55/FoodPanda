783be7423f445147b0997e00347ebc845517e967
code <vaibhavthakare375@gmail.com>
2023-09-10 13:39:44 +0530

    ecomarce

---
+++ src/app/pages/menu/menu.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import { OrderDetailsService } from 'src/app/service/order-details.service';
+OrderDetailsService
+@Component({
+  selector: 'app-menu',
+  templateUrl: './menu.component.html',
+  styleUrls: ['./menu.component.css']
+})
+export class MenuComponent implements OnInit {
+
+  constructor(private service:OrderDetailsService) { }
+  foodData:any;
+  ngOnInit(): void {
+    this.foodData = this.service.foodDetails;
+  }
+
+}
